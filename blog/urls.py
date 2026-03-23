ee8b868e61a2cd9eaac6437b7add2c9f5c3b6c77
sifaz <sifazhang.nz@gmail.com>
2026-03-24 09:22:22 +1300

    week5 practice

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
 
-from blog.views import index
+from blog.views import index, category_detail, post_detail, create_category, create_post, PostListView, PostDetailView
 
 urlpatterns = [
     path('', index, name='index'),
+    path('post/<int:post_id>', post_detail, name='post_detail'),
+    path('category/<int:category_id>', category_detail, name='category_detail'),
+
+    path('create_category', create_category, name='create_category'),
+
+    path('create_post', create_post, name='create_post'),
+    path('posts/', PostListView.as_view(), name='post_list'),
+
+    path('post_detail/<int:pk>/detail', PostDetailView.as_view(), name='post_detail_view'),
 ]
+
+
